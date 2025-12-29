8786df03dfd6ff60754988a9c34228014c486b4b
NotAnxious <jinlu920@163.com>
2025-12-29 20:02:21 +0800

    a commit

--- Space-War/ene.h
+++ Space-War/ene.h
@@ -4,18 +4,13 @@
 #include <vector>
 #include <main.h>
 using namespace std;
-vector<int> eneX = {0,100};
-vector<int> eneY = {0,100};
+vector<int> eneX = { 100,200 };
+vector<int> eneY = { 200,100 };
 IMAGE enePng;
 
 void RefreshENEPosit() {
 	for (int i = 0; i < eneY.size(); ++i) {
-		eneY[i] -= 1.0 / 30.0;
-	}
-}
-void showENE() {
-	for (int i = 0; i < eneX.size(); ++i) {
-		showImage(NULL, eneX[i], eneY[i], &enePng);
+		eneY[i] += 1.0;
 	}
 }
 
--- Space-War/main.cpp
+++ Space-War/main.cpp
@@ -7,11 +7,12 @@
 #include <Windows.h>
 #include <cstdlib>
 #include <ctime>
+#include <chrono>
 #include <ene.h>
 #include <ship.h>
 #include <main.h>
 #pragma comment(lib,"msimg32.lib")
-#pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
+// #pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
 
 using namespace std;
 
@@ -33,7 +34,8 @@ int main() {
 
 	// 初始化变量
 	HDC hdc = GetDC(NULL);
-	clock_t Tstart, Tend;// 计时器
+	auto Tstart = chrono::high_resolution_clock::now();// 计时器
+	auto Tend = chrono::high_resolution_clock::now();// 计时器
 	srand(time(0));// 初始化时间种子方便生成随机数
 
 	HWND gameHWND = FindWindow(NULL,L"Space-War");// 获取游戏窗口句柄
@@ -63,7 +65,7 @@ int main() {
 	spdlog::info("Game window initialized successfully");
 
 	// 初始化窗口
-	LONG l_WinStyle = GetWindowLong(hwnd, GWL_STYLE); // 获取窗口信息
+	const LONG l_WinStyle = GetWindowLong(hwnd, GWL_STYLE); // 获取窗口信息
 	// 设置窗口信息 最大化 取消标题栏及边框
 	SetWindowLong(hwnd, GWL_STYLE, (l_WinStyle | WS_POPUP | WS_MAXIMIZE) & ~WS_CAPTION & ~WS_THICKFRAME & ~WS_BORDER);// 直接修改窗口样式
 	SetWindowPos(hwnd, HWND_TOP, 0, 0, rW, rH, 0);
@@ -75,7 +77,7 @@ int main() {
 	// 运行主循环
 	spdlog::info("Entering main game loop");
 	while (true) {
-		Tstart = clock();// 开始计时
+		Tstart = chrono::high_resolution_clock::now();// 开始计时
 		BeginBatchDraw(); // 开始双缓冲
 		cleardevice(); // 清屏
 		// 获取屏幕大小
@@ -86,6 +88,9 @@ int main() {
 		showImage(NULL, 0, 0, &starrySkyPng);// 星空背景(最后置)
 		showImage(NULL, 0, cy - 217, &LPortalPng);// 左传送门
 		showImage(NULL, cx - 150, cy - 217, &RPortalPng);// 右传送门
+		for (int i = 0; i < eneX.size(); ++i) {
+			showImage(NULL, eneX[i], eneY[i], &enePng);
+		}
 		showImage(NULL, shipX, cy - 204, &myShipPng);// 我方飞船(最前置)
 		
 		// 按下esc键就退出
@@ -98,6 +103,12 @@ int main() {
 		if (shipX <= -126) { shipX = cx; }// 超过左边
 
 		EndBatchDraw(); // 结束双缓冲绘图
-		Sleep(1);
+		Tend = chrono::high_resolution_clock::now();// 结束计时
+		// 计算时间
+		chrono::duration<double, milli> duration = Tend - Tstart;
+		if (int(duration.count()) < 17) {
+			cout << 17 - int(duration.count())<<endl;
+			Sleep(17 - int(duration.count()));
+		}
 	}
 }
